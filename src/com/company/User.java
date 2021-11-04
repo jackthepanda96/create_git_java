8805a01c70f62e88aad0f1736cac843a2ba7091e
Jerry Young <jerryyoung2911@gmail.com>
2021-11-04 19:59:11 +0700

    Print Hello World

---
+++ src/com/company/User.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class User {
+    String HP;
+    String nama;
+
+    public String getHP() {
+        return HP;
+    }
+
+    public void setHP(String HP) {
+        this.HP = HP;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+}
